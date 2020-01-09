0688baa58ca10ac64d82e06aa5602324abfcd585
pavelp21 <pavelvp2182@yandex.ru>
2020-01-09 14:17:23 +0500

    Main page is adapted

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,6 +1,8 @@
 //@prepros-prepend jquery-2.1.1.min.js
 //@prepros-prepend bootstrap.min.js
+//@prepros-prepend flipclock.min.js
 //@prepros-prepend jquery.fancybox.min.js
+
 var isIE = false || !!document.documentMode;
 
 if (isIE) {
@@ -24,6 +26,41 @@ $(document).ready(function () {
             }, 1500)
         });
     });
+    $(function () {
+        /* Таймер */
+        var clock;
+        var futureDate = new Date("January 13, 2020 00:00 AM UTC+3");
+        var currentDate = new Date();
+        var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+        function dayDiff(first, second) {
+            return (second - first) / (1000 * 60 * 60 * 24);
+        }
+        if (dayDiff(currentDate, futureDate) < 100) {
+            $('.clock').addClass('twoDayDigits');
+        } else {
+            $('.clock').addClass('threeDayDigits');
+        }
+        if (diff < 0) {
+            diff = 0;
+            $('#link').attr('href', 'https://shop.mastervision.su/?r=ordering/cart/as1&id=4365&clean=true&lg=ru');
+            $('.cost-full span').addClass('unbroken');
+            $('.timer, .cost-sale, .cost-today, .prepayment').remove();
+        }
+        clock = $('.clock').FlipClock(diff, {
+            clockFace: 'HourlyCounter',
+            countdown: true,
+            language: 'ru',
+            callbacks: {
+                stop: function () {
+                    $('#link').attr('href', 'https://shop.mastervision.su/?r=ordering/cart/as1&id=4365&clean=true&lg=ru');
+                    $('.cost-full span').addClass('unbroken');
+                    $('.timer, .cost-sale, .cost-today, .prepayment').remove();
+                }
+            },
+        });
+    });
+    /*Видео в отзывах*/
     $(".video-wrapper-revs img").click(function () {
         var a = $(this).parent().attr("data-youtube");
         $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1"  allowfullscreen></iframe>')
@@ -32,7 +69,7 @@ $(document).ready(function () {
         $('#video-revs').on('slide.bs.carousel', function () {
             $('.video-wrapper-revs iframe').each(function () {
                 var l = $(this).parent().attr('data-img');
-                $(this).parent().html('<img class="video_rev_img" src="' + l + '" alt="Видео отзыв">');
+                $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
             });
             $(".video-wrapper-revs img").click(function () {
                 var a = $(this).parent().attr("data-youtube");
